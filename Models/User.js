22de601778c5ddc4e51f388f98309702fb6016ab
Anandarun21 <anandarun238@gmail.com>
2024-03-14 09:52:43 +0530

    init

--- Models/User.js
+++ Models/User.js
@@ -1,23 +1,26 @@
 const Sequelize = require('sequelize');
-
 const sequelize = require('../util/database');
 
-const User = sequelize.define('user',{
-    id:{
-        type:Sequelize.INTEGER,
-        autoIncrement:true,
-        allowNull:false,
-        primaryKey:true
+const User = sequelize.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
     },
-    Name:{
-        type:Sequelize.STRING,
-        allowNull:false
+    Name: {
+        type: Sequelize.STRING,
+        allowNull: false
     },
-    Mail:{
-        type:Sequelize.STRING,
-        allowNull:false,
-        unique:true
+    Mail: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    Phone: { // Adding Phone field
+        type: Sequelize.STRING, // Assuming phone numbers are stored as strings
+        allowNull: false
     }
 });
 
-module.exports= User;
+module.exports = User;
